37af0512041103f039e9b926d067ff84d653d1b5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-03-18 14:01:19 +0200

    ...

--- Web cloning/script.js
+++ Web cloning/script.js
@@ -5,3 +5,15 @@ document.querySelectorAll(".navbar li").forEach(item => {
     item.classList.add("active");
   });
 });
+
+document.querySelectorAll(".socials i").forEach(item => {
+  item.addEventListener("click", () => {
+    alert("This is a demo website. Social media links are not available.");
+  });
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  alert("This is a demo website. Services are not available.");
+});
+
+document.querySelector()
