72cc532066388883fa13de29424d8413e9262e51
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-03-30 11:08:05 +0200

    Laugh it Off

--- course/LiO.js
+++ course/LiO.js
@@ -1,8 +1,8 @@
-function laugh(num){
+var ha = function laugh(num){
       let result = "";
       for (let i = 0; i < num; i++) {
             result += "ha";
       }
       return result + "!";
 }
-console.log(laugh(3))
+console.log(ha(3))
