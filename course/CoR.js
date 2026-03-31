829afb8f6f33d7e9ff7236e00b955e18e22230a5
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-03-31 15:22:37 +0200

    Colors of the Rainbow

--- course/CoR.js
+++ course/CoR.js
@@ -11,5 +11,3 @@ var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue'];
 rainbow.splice(2,1,"Yellow", "Green")
 rainbow.splice(rainbow.length, 0 , "Purple")
 console.log(rainbow)
-
-// your code goes here
