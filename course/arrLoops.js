28677cbeb87e1f6490a4f2719ca88d251435a404
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-03-31 16:01:42 +0200

    Array Loops

--- course/arrLoops.js
+++ course/arrLoops.js
@@ -4,3 +4,8 @@ for (var i = 0; i < people.length; i++) {
    people[i] = "Dr. " + people[i].toUpperCase()
    console.log(people[i]);
 }
+
+people.forEach(function(person) {
+      person += "(the great)";
+      console.log(person);
+})
