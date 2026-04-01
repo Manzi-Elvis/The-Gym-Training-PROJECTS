0d60f26c7358c5b96bbcaccff93c691343e3549d
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-04-01 15:11:23 +0200

    tripple trouble

--- Codewars/trippleTrouble.js
+++ Codewars/trippleTrouble.js
@@ -1,7 +1,7 @@
 function tripleTrouble(one, two, three){
-      let result = [];
+      let result = "";
       for (let i = 0; i < one.length; i++) {
-            result.push(one[i] + two[i] + three[i]);
+            result += one[i] + two[i] + three[i];
       }
       return result;
   
