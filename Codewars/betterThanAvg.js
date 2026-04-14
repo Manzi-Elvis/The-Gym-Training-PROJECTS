f760057788cc01512076eec21b327b85c1d60a1f
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-04-14 15:22:10 +0200

    Calculate average

--- Codewars/betterThanAvg.js
+++ Codewars/betterThanAvg.js
@@ -3,3 +3,26 @@ function betterThanAverage(classPoints, yourPoints) {
   const avg = classPoints.reduce((a,b) => a + b, 0) / classPoints.length;
   return yourPoints > avg;
 }
+/*
+        \                                /
+         \                              /
+          \ A Developer Journey Begins /
+           ]  with a coding problem    [ 
+           ]                     [   /  |
+           ]___               ___[ ,'   |
+           ]  ]\             /[  [ |:   |
+           ]  ] \           / [  [ |:   |
+           ]  ]  ]         [  [  [ |:   |
+           ]  ]  ]__     __[  [  [ |:   |
+           ]  ]  ] ]\ _ /[ [  [  [ |:   |
+           ]  ]  ] ] (#) [ [  [  [ :===='
+           ]  ]  ]_].nHn.[_[  [  [
+           ]  ]  ]  HHHHH. [  [  [
+           ]  ] /   `HH("N  \ [  [
+           ]__]/     HHH  "  \[__[
+           ]         NNN         [
+           ]         N/"         [
+           ]         N H         [
+          /          N            \
+         /           q,            \
+        /                           \ */
