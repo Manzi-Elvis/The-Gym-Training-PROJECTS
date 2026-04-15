fe6eb294a585785a74c97d1250d70934b6771d52
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-04-15 08:50:20 +0200

    Sum of Odd Numbers

---
+++ Codewars/SumofOddNum.js
@@ -0,0 +1,8 @@
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
+console.log(rowSumOddNumbers(1)); // 1
+console.log(rowSumOddNumbers(2)); // 8
+console.log(rowSumOddNumbers(3)); // 27
+console.log(rowSumOddNumbers(4)); // 64
+console.log(rowSumOddNumbers(5)); // 125
