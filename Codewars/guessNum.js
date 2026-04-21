4a9e923ae040dfaa3ff89c25072a6db71e6b9b71
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-04-21 15:57:05 +0200

    guess the number

--- Codewars/guessNum.js
+++ Codewars/guessNum.js
@@ -5,6 +5,18 @@ class Guesser {
   }
   
   guess(n) {
+    if (this.lives <= 0) {
+      return false;
+    }
+    if (n === this.number) {
+      return true;
+    }
+    this.lives--;
     return false;
   }
 }
+console.log(new Guesser(10, 3).guess(10)); // Output: true
+console.log(new Guesser(10, 3).guess(5)); // Output: false
+console.log(new Guesser(10, 3).guess(5)); // Output: false
+console.log(new Guesser(10, 3).guess(5)); // Output: false
+console.log(new Guesser(10, 3).guess(5)); // Output: false (no lives left)
