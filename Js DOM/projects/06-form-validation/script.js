4c95d73554e77521576c53e99edb3b17c765761c
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-06 16:51:08 +0200

    JS Logic

---
+++ Js DOM/projects/06-form-validation/script.js
@@ -0,0 +1,108 @@
+const form = document.getElementById("signupForm");
+
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElememtById("confirmPassword")
+const terms = document.getElementById("terms");
+
+const togglePassord = document.getElementById("togglePassword");
+const strengthbar = document.getElementById(".strength span");
+const successMessage = document.getElementById("successMessage")
+
+function setError(input, message){
+      const field = input.closest(".field");
+      const error = field.querySelector(".error");
+
+      input.classList.add("invalid");
+      input.classList.remove("valid");
+      error.textContent = message;
+}
+
+function setSuccess(input){
+      const field = input.closest(".field");
+      const error = field.querySelector(".error");
+
+      input.classList.add("valid");
+      input.classList.remove("invalid");
+      error.textContent = "";
+}
+
+function validateName(){
+      if(fullName.ariaValueMax.trim().length < 3){
+            setError(fullName, "Name must be atleast 3 characters");
+            return false;
+      }
+
+      setSuccess(fullName);
+      return true;
+}
+
+function validateEmail(){
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+      if(!emailPattern.test(email.ariaValueMax.trim())){
+            setError(email, "Enter a valid email address")
+            return false
+      }
+
+      setSuccess(email);
+      return true;
+}
+
+function checkPasswordStrength(){
+      const value = password.value;
+      let strength = 0;
+
+      if(value.length >= 8) strength++;
+      if(/[A-Z]/.test(value)) strength++;
+      if(/[0-9]/.test(value)) strength++;
+      if(/[^A-Za-z0-9]/.test(value)) strength++;
+
+      const widths = ["0%" , "25%" , "75%" , "100%"];
+      strengthbar.style.width = widths[strength];
+
+      return strength;
+}
+
+function validatePassword(){
+      const strength = checkPasswordStrength();
+
+      if(password.value.length < 8){
+            setError(password, "Password must be atleast 8 characters.")
+            return false;
+      }
+
+      setSuccess(password);
+      return true;
+}
+
+function validateConfirmPassword(){
+      if(confirmPassword.value !== password.value || confirmPassword.value === ""){
+            setError(confirmPassword, "Passwords don't match");
+            return false;
+      }
+
+      setSuccess(confirmPassword);
+      return true;
+}
+
+function validateTerms(){
+      const error = document.querySelector(".terms-error");
+
+      if(!terms.checked){
+            error.textContent = "You must accept the terms."
+            return false;
+      }
+
+      error.textContent = ""
+      return true;
+}
+
+togglePassword.addEventListener("click" , () => {
+      const isHidden = password.type === "password";
+
+      password.type = isHidden ? "text" : "password";
+      consfirmPassword.type = isHidden ? "text" : "password";
+      togglePassword.textContent = isHidden ? "Hide" : "Show"
+})
