6b547875cd8b1364af998ccd84ab604d47aa4b93
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-06 14:05:57 +0200

    JS Logic

---
+++ Js DOM/projects/04-tabs/script.js
@@ -0,0 +1,21 @@
+const tabButtons = document.querySelectorAll(".tab-btn")
+const tabContent = document.querySelectorAll(".tab-content")
+
+tabButtons.forEach((button) => {
+      button.addEventListener("click" , () => {
+            const selectedTab = button.dataset.tab;
+
+            tabButtons.forEach((btn) => {
+                  btn.classList.remove("active")
+            });
+
+            tabContents.forEach((content) => {
+                  content.classList.remove("active")
+            });
+            
+            button.classList.add("active");
+
+            const activeContent = document.getElementById(selectedTab);
+            activeContent.classList.add("active")
+      });
+});
