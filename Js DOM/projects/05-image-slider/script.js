414ced4bab7b3a5817f47bd4ddcf47f3e52d2f7a
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-06 15:30:47 +0200

    JS Logic

--- Js DOM/projects/05-image-slider/script.js
+++ Js DOM/projects/05-image-slider/script.js
@@ -2,12 +2,26 @@ const sliderImage = document.getElementById("sliderImage")
 const prevBtn = document.getElementById("prevBtn")
 const nextBtn = document.getElementById("nextBtn")
 
-const images = ["images/img1.jpg" , "images/img2.jpg" , "images/img3.jpg"];
+const images = [
+      {
+            image: "images/img1.jpg",
+            background: "#444"
+      },
+      {
+            image: "images/img2.jpg",
+            background: "#1B4332"
+      },
+      {
+            image: "images/img3.jpg",
+            background: "#ffffff"
+      }
+];
 
 let currentIndex = 0;
 
 function showImage(){
-      sliderImage.src = images[currentIndex]
+      sliderImage.src = images[currentIndex].image;
+      document.body.style.background = images[currentIndex].background;
 }
 
 nextBtn.addEventListener("click" , () => {
