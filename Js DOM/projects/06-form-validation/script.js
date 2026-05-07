4108b438d2de8d5209ab29fa1ccbe3b0e404474b
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-07 11:05:37 +0200

    JS Logic

--- Js DOM/projects/06-form-validation/script.js
+++ Js DOM/projects/06-form-validation/script.js
@@ -39,7 +39,7 @@ function validateName(){
 }
 
 function validateEmail(){
-      const emailPattern = "/^[^\s@]+@[^\s@]+\.[^\s@]+$/";
+      const emailPattern = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
 
       if(!emailPattern.test(email.value.trim())){
             setError(email, "Enter a valid email address")
