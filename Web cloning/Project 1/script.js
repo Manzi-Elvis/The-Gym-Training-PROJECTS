2b1681f52e6ab9034d9ac462a17ef1f37da1bdac
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-11 10:05:31 +0200

    moved

---
+++ Web cloning/Project 1/script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll(".navbar li").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelector(".active").classList.remove("active");
+    item.classList.add("active");
+  });
+});
+
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 10) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+
+  const sections = document.querySelectorAll("section");
+  const scrollPos = window.scrollY + navbar.offsetHeight;
+      sections.forEach(section => {
+            if (scrollPos >= section.offsetTop && scrollPos < section.offsetTop + section.offsetHeight) {
+                  document.querySelector(".active").classList.remove("active");
+                  const id = section.getAttribute("id");
+                  const activeLink = document.querySelector(`.navbar a[href="#${id}"]`);
+                  if (activeLink) {
+                        activeLink.parentElement.classList.add("active");
+                  }
+            }
+      });
+});
