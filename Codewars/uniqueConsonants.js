e69157893c3be286149ec2a6e72cc0d95270ee95
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-18 15:46:51 +0200

    How Many Unique Consonants?

--- Codewars/uniqueConsonants.js
+++ Codewars/uniqueConsonants.js
@@ -1,11 +1,5 @@
 function countConsonants(str) {
-      const vowels = "aeiou";
       const unique = [];
-      let count = 0;
-      str.toLowerCase().split('').map(char =>{
-            if(char >= "a" && char <= "z" && !vowels.includes(char) && !unique.includes(char) ){
-                  unique.push(char);
-            }
-      })
-    return unique.length;
+      str.toLowerCase().split('').map(char => {if(char >= "a" && char <= "z" && !"aeiou".includes(char) && !unique.includes(char) )unique.push(char);})
+      return unique.length;
 }
