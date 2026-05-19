e50915e8724339fa8cedd7c776ed184beb55fbb2
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-19 11:24:50 +0200

    renamed

--- DeepJS/JS Fundamentals/isMirror.js
+++ DeepJS/JS Fundamentals/isMirror.js
@@ -3,7 +3,7 @@
 
 function isMirrorImage(str1, str2){
       function clean(str){
-            str.toLowerCase().replace(/[^a-z0-9]/g, "")
+            return str.toLowerCase().replace(/[^a-z0-9]/g, "")
       }
       const s1 = clean(str1);
       const s2 = clean(str2);
@@ -11,7 +11,7 @@ function isMirrorImage(str1, str2){
       const reverseds1 = s1.split('').reverse().join('')
       return reverseds1 === s2
 }
-
-isMirrorImage("Hello", "olleH"); // true
-isMirrorImage("World", "dlroW"); // true
-isMirrorImage("Test", "tseT "); // false
+console.log(isMirrorImage("Hello", "olleH")); // true
+console.log(isMirrorImage("World", "dlroW")); // true
+console.log(isMirrorImage("Test", "tseT ")); // true
+console.log(isMirrorImage("Java", "Python")); // false
