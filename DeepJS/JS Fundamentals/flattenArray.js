65887ccad5f288a9067fd095a30fa41b6b196899
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-19 10:06:03 +0200

    flatten Array using recursion

---
+++ DeepJS/JS Fundamentals/flattenArray.js
@@ -0,0 +1,17 @@
+// Write a function flattenArray(arr) that flattens a nested array of any depth
+//  into a single array without using Array.prototype.flat(). Use recursion.
+
+function flattenArray(arr){
+      let result = [];
+      for(let i = 0; i < arr.length; i++){
+            if(Array.isArray(arr[i])){
+                  result = result.concat(flattenArray(arr[i]));
+            }
+            else{
+                  result.push(arr[i]);
+            }
+      }
+      return result;
+}
+
+console.log(flattenArray([1, [2, [3, [4]], 5]])); // [1, 2, 3, 4, 5]
