9b344b5138b2e5b95994ee10b8a4b57631f2ce73
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-28 10:23:21 +0200

    Sets

--- ES6/sets.js
+++ ES6/sets.js
@@ -21,3 +21,15 @@ console.log(games); // PRINTS: Set {'Banjo-Kazooie', 'Mario Kart', 'Banjo-Tooie'
 
 games.clear()
 console.log(games); // PRINTS: Set {}
+
+// CHECKING LENGTH: 
+const months = new Set(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+console.log(months.size); // PRINTS: 12
+
+// Checking If An Item Exists, use the .has() method
+
+console.log(months.has('September')); // true
+
+// Retrieving All Values, use the .values() method to return the values in a Set.
+console.log(months.values());
+// SetIterator {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'}
