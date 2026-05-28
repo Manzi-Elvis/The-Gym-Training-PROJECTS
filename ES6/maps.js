b6dfce1c56ccde3cf684b0c3b4bc61d5c0bd05db
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-05-28 11:02:51 +0200

    Working with Maps

--- ES6/maps.js
+++ ES6/maps.js
@@ -34,3 +34,21 @@ console.log(employees); // Map {'james.parkes@udacity.com' => Object {firstName:
 // use the .clear() method to remove all key-value pairs from the Map.
 employees.clear()
 console.log(employees);// Map {}
+
+
+
+// Working with Maps:
+// use the .has() method to check if a key-value pair exists in your Map by passing it a key.
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+console.log(members.has('Xavier')); // false
+console.log(members.has('Marcus')); // true
+
+// retrieve values from a Map, by passing a key to the .get() method.
+
+console.log(members.get('Evelyn')); // PRINTS: 75.68
