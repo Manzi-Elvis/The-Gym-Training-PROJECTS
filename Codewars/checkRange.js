b33e60292a8d7458f86fc087f4cac632b8824819
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-07-09 08:52:46 +0200

    One Line Task: Check Range(Accepted Approach)

--- Codewars/checkRange.js
+++ Codewars/checkRange.js
@@ -15,7 +15,8 @@ the output should be 7.
 elements 2, 5, 6, 7, 1, 3, 4 should be counted.
 */
 
-checkRange = (a, x, y)=>a.filter(v => x <= v & v <= y).length
+checkRange=(a,x,y,c=0)=>a.map(v=>c+=v<x==v>y)|c
+checkRangeMe=(a,x,y)=>a.filter(v=>x<=v&v<=y).length
 
 checkRange([2,5,6,7,1,3,4,11,56,49],1,7) // 7
 checkRange([1,2,3,4,5],2,4) // 3
