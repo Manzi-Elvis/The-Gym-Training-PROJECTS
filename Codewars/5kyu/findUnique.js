d1706f7cefc3456fa8a94ffb7b09e0354723715b
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-08-03 15:02:17 +0200

    Find the unique string

--- Codewars/5kyu/findUnique.js
+++ Codewars/5kyu/findUnique.js
@@ -18,6 +18,15 @@ function findUniq(arr) {
   const result = arr.map(s => [...new Set(s.toLowerCase())].sort().join(''));
   return arr[result.findIndex(p => result.indexOf(p) === result.lastIndexOf(p))]
 }
+
+// OTHER APPROACHES:
+
+findUniq2=(a,b=a.map(a=>[[...new Set(a.toLowerCase())].sort().join``,a]),c=b.map(a=>a[0]))=>b[c.findIndex(a=>c.indexOf(a)==c.lastIndexOf(a))][1]
+function findUniq3(arr) {
+   return arr.sort().filter((e, i, a) => e.match(new RegExp(`[^${a[1]}]`, 'gi')))[0];
+}
+let findUniq4 = xs => xs.find((x, i) => [...x].some(c => !xs.some((y, j) => i !== j && new RegExp(c, 'i').test(y))));
+
 // TEST CASES
 console.log(findUniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a'])); // "BbBb"
 console.log(findUniq(['abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba'])); // "foo"
