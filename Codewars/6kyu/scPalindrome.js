4aa4796a6273636522870a0a25eb62b7c1cf93af
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2026-08-03 15:34:31 +0200

    Single character palindromes

--- Codewars/6kyu/scPalindrome.js
+++ Codewars/6kyu/scPalindrome.js
@@ -1,3 +1,22 @@
+/*
+Description:
+You will be given a string and you task is to check if it is possible
+to convert that string into a palindrome by removing a single character.
+If the string is already a palindrome, return "OK". If it is not, and
+we can convert it to a palindrome by removing one character, then return "remove one",
+otherwise return "not possible". The order of the characters should not be changed.
+
+For example:
+
+"abba"   -> "OK"           - this is a palindrome
+"abbaa"  -> "remove one"   - remove the 'a' at the extreme right
+"abbaab" -> "not possible" - cannot be made a palindrome 
+Good luck!
+
+If you like this Kata, please try Single Character Palindromes II
+*/
+
+
 function solve(s){
       const isPalindrome = (str) => {
             const rev = str.split('').reverse().join('')
